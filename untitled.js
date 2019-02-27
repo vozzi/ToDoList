d29f454978cd9b6fff910b94788928424ed08210
vozzi <37548527+vozzi@users.noreply.github.com>
2019-02-27 11:24:44 +0600

    Update untitled.js

--- untitled.js
+++ untitled.js
@@ -42,7 +42,7 @@ function newElement() {
 
   for (let i = 0; i < close.length; i++) {
     close[i].onclick = function() {
-      var div = this.parentElement;
+      let div = this.parentElement;
       div.style.display = "none";
     }
   }
